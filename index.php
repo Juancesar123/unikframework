66caea6c29baf66ee5e6b51d371de831ef7b256d
juancesar123 <juancesarandrianto@gmail.com>
2018-10-21 21:16:45 +0700

    added routes SSR

--- index.php
+++ index.php
@@ -19,3 +19,4 @@ require __DIR__ . '/vendor/autoload.php';
 $log = new Monolog\Logger('name');
 $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
 $log->addWarning('Foo');
+require("route/web.php");
