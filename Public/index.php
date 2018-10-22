bfb53a5fedbdcfca2be5e11ba0a450fc993fdca5
juancesar123 <juancesarandrianto@gmail.com>
2018-10-22 12:41:36 +0700

    deleted unused folder

--- Public/index.php
+++
@@ -1,6 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-$log->addWarning('Foo');
--- app/Core/Irequest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Core;
-
-
-interface IRequest
-{
-    public function getBody();
-}
